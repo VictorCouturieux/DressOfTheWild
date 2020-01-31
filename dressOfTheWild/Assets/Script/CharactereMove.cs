e240cf8469e6d80b86c7d02c1c45c89f9385a585
VictorCouturieux <victor.couturieux.auditeur@lecnam.net>
2020-02-01 00:01:01 +0100

    add CharachereControler on SceneTOTOR and Xinput lib

---
+++ dressOfTheWild/Assets/Script/CharactereMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class CharactereMove : MonoBehaviour {
+    public float speed = 6f;
+
+    
+    bool playerIndexSet = false;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+    
+    
+    private Vector3 moveDirection = Vector3.zero;
+    CharacterController Cc;
+
+    // Start is called before the first frame update
+    void Start() {
+        Cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        
+        
+        
+    }
+}
