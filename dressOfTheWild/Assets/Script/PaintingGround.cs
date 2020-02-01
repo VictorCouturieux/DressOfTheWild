77b5f9f8bd30f5080ed32ed61f114fa8e0540052
RIBOUT <pifi77@hotmail.fr>
2020-02-01 13:26:40 +0100

    Palette on big terrain

--- dressOfTheWild/Assets/Script/PaintingGround.cs
+++ dressOfTheWild/Assets/Script/PaintingGround.cs
@@ -4,8 +4,43 @@ using UnityEngine;
 
 public class PaintingGround : MonoBehaviour
 {
+    public float lenghtOfThatRay = 1.5f;
+    public LayerMask layerMaskIfNeeded;
+
+    MeshFilter meshMesh;
+
     public void RaycastGround()
     {
+        RaycastHit hitRay;
 
+        Vector3 startPositionForTheRay = this.transform.position;
+        Vector3 direction = Vector3.down * lenghtOfThatRay;
+
+        Ray rayForWall = new Ray(startPositionForTheRay, direction);//use the current upVector to check.
+#if UNITY_EDITOR
+        Debug.DrawRay(startPositionForTheRay, direction, Color.cyan);
+#endif
+        if (Physics.Raycast(rayForWall, out hitRay, lenghtOfThatRay))
+        {
+            int indexTriangle = hitRay.triangleIndex;
+            Vector3 barycenterPoint = hitRay.barycentricCoordinate;
+
+            //else, fetch one the mesh.
+            if (meshMesh == null || meshMesh.name != hitRay.collider.name)
+                meshMesh = hitRay.collider.GetComponent<MeshFilter>();
+
+            int[] triangles = meshMesh.mesh.triangles;
+            Color[] colorVertex = meshMesh.mesh.colors;
+            
+            colorVertex[triangles[indexTriangle * 3 + 0]] = Color.green;
+            colorVertex[triangles[indexTriangle * 3 + 1]] = Color.green;
+            colorVertex[triangles[indexTriangle * 3 + 2]] = Color.green;
+
+            meshMesh.mesh.colors = colorVertex;
+        }
+
+
+
+        
     }
 }
