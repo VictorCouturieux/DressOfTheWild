bbd5cd9b45c9e901eb63f5d828c4667f49d15850
VictorCouturieux <victor.couturieux.auditeur@lecnam.net>
2020-02-01 10:55:28 +0100

    add charachere prefab

--- dressOfTheWild/Assets/Script/CharactereMove.cs
+++ dressOfTheWild/Assets/Script/CharactereMove.cs
@@ -23,9 +23,45 @@ public class CharactereMove : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+
+//        if (!playerIndexSet || !prevState.IsConnected)
+//        {
+//            for (int i = 0; i < 4; ++i)
+//            {
+//                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+//                GamePadState testState = GamePad.GetState(testPlayerIndex);
+//                if (testState.IsConnected)
+//                {
+//                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+//                    playerIndex = testPlayerIndex;
+//                    playerIndexSet = true;
+//                }
+//            }
+//        }
         
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
         
+        moveDirection = new Vector3(state.ThumbSticks.Left.X,0,state.ThumbSticks.Left.Y);
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
         
+        transform.localRotation *= Quaternion.Euler(Camera., 0.0f, 0.0f);
         
+        Cc.Move(moveDirection * Time.deltaTime);
     }
+    
+    void OnGUI()
+    {
+        string text = "Use left stick to turn the cube, hold A to change color\n";
+        text += string.Format("IsConnected {0} Packet #{1}\n", state.IsConnected, state.PacketNumber);
+        text += string.Format("\tTriggers {0} {1}\n", state.Triggers.Left, state.Triggers.Right);
+        text += string.Format("\tD-Pad {0} {1} {2} {3}\n", state.DPad.Up, state.DPad.Right, state.DPad.Down, state.DPad.Left);
+        text += string.Format("\tButtons Start {0} Back {1} Guide {2}\n", state.Buttons.Start, state.Buttons.Back, state.Buttons.Guide);
+        text += string.Format("\tButtons LeftStick {0} RightStick {1} LeftShoulder {2} RightShoulder {3}\n", state.Buttons.LeftStick, state.Buttons.RightStick, state.Buttons.LeftShoulder, state.Buttons.RightShoulder);
+        text += string.Format("\tButtons A {0} B {1} X {2} Y {3}\n", state.Buttons.A, state.Buttons.B, state.Buttons.X, state.Buttons.Y);
+        text += string.Format("\tSticks Left {0} {1} Right {2} {3}\n", state.ThumbSticks.Left.X, state.ThumbSticks.Left.Y, state.ThumbSticks.Right.X, state.ThumbSticks.Right.Y);
+        GUI.Label(new Rect(0, 0, Screen.width, Screen.height), text);
+    }
+    
 }
