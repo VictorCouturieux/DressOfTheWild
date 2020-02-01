cf9d1c391700b03a0c6fe6c9578a08326437e145
RIBOUT <pifi77@hotmail.fr>
2020-02-01 14:49:19 +0100

    Face Camera

---
+++ dressOfTheWild/Assets/Script/FaceCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    Transform visual;
+    Transform mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (CameraMain.instance == null)
+            GameObject.Find("Main Camera").AddComponent<CameraMain>();
+
+
+        mainCamera = CameraMain.instance.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       this.transform.localRotation = new Quaternion(
+            mainCamera.transform.rotation.x,
+            mainCamera.transform.rotation.y,
+            mainCamera.transform.rotation.z,
+             transform.rotation.w);
+    }
+}
